2b3c7ad533597e114d8081d92933a445a5b1d6ab
0xlvl3 <cryptochoccy@gmail.com>
2022-12-28 21:58:04 +1100

    Workouts now saving to specified directory + workout saved in JSON

--- current_workout.py
+++ current_workout.py
@@ -2,28 +2,80 @@ from workout import Workout
 from eset import Eset
 
 import time
+from datetime import date
+import datetime
+import json
+import os
+
+# Check if user wants to add workout to a current directory within workout_data
+# If they want to use directory that exists create a new json file
+# If not create directory, then create the JSON file
+# Workout class name will also be directory that JSON file for a Eset gets saved to
+# Details about the workout within Workout class will be saved in the same JSON file as Eset class
 
 
 def working_out():
     start = time.time()
+    now = datetime.datetime.now()
+    formatted_time = now.strftime("%I:%M:%S_%p")
+    print(formatted_time)
+    current_date = date.today()
 
-    while True:
-        add_new = input("Add another set? (y/n)").lower()
-        if add_new == "y":
-            exercise = input("Exercise name: ").lower()
-            set_count = int(input("Set number: "))
-            reps = int(input("Reps: "))
-            weight = int(input("Weight: "))
-            print(
-                f"Exercise: {exercise} set number {set_count}, for {reps} reps at {weight} weight"
-            )
-        if add_new == "n":
-            break
-    end = time.time()
+    workout_name = input("Name your workout: ").lower()
+    # Check if directory exists
+    if not os.path.exists(f"workout_data/{workout_name}"):
+        os.mkdir(f"workout_data/{workout_name}")
+        with open(
+            f"workout_data/{workout_name}/workout_{current_date}_{formatted_time}.json",
+            "w",
+        ) as file:
+            workout = {}
+            json.dump(workout, file)
+    elif os.path.exists(f"workout_data/{workout_name}"):
+        with open(
+            f"workout_data/{workout_name}/workout_{current_date}_{formatted_time}.json",
+            "w",
+        ) as file:
+            workout = {}
+            json.dump(workout, file)
 
-    elapsed = end - start
+    print("Workout starting..")
+    time.sleep(1)
+    print("Workout starting...")
+    time.sleep(1)
+    print("Workout starting....")
+    print(f"{current_date}")
 
-    print(f"Elapsed time: {elapsed}")
+    with open(
+        f"workout_data/{workout_name}/workout_{current_date}_{formatted_time}.json",
+        "a",
+    ) as f:
+        while True:
+            add_new = input("Add another set? (y/n)").lower()
+            if add_new == "y":
+                exercise = input("Exercise name: ").lower()
+                set_count = int(input("Set number: "))
+                reps = int(input("Reps: "))
+                weight = int(input("Weight: "))
+                print(
+                    f"Exercise: {exercise} set number {set_count}, for {reps} reps at {weight} weight"
+                )
+
+                add_set = Eset(exercise, set_count, reps, weight)
+
+                # Serialize the object using json.dump function
+                add_set_data = json.dumps(add_set.__dict__, indent=4)
+                print(f"{add_set} added")
+
+                f.write(add_set_data)
+
+            elif add_new == "n":
+                break
+        end = time.time()
+
+        elapsed = end - start
+
+        print(f"Elapsed time: {elapsed}")
 
 
 working_out()
